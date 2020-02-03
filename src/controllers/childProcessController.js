d4a7e206421a9045652b76953bd7159040fafa55
Pritesh Shinde <priteshs@prdxn.com>
2020-02-04 00:42:14 +0530

    Child process created for executing JAR file

--- src/controllers/childProcessController.js
+++ src/controllers/childProcessController.js
@@ -11,3 +11,14 @@ exports.executePHP = (req, res) => {
     return res.send(stdout);
   });
 }
+
+exports.executeJar = (req, res) => {
+  exec(`java -jar ${path.join(path.dirname(process.mainModule.filename))}/Hello.jar ${req.params.name}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+    return res.send(stdout);
+  });
+}
